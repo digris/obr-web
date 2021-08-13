cf9200cae4e03f0c4b1f011577882de258f6154b
Jonas Ohrstrom <ohrstrom@gmail.com>
2021-08-13 15:26:07 +0200

    Reworking "live-color" / page color / theme.
    Former-commit-id: cc161ea124e0045d6f982092bbfcc422b2b3b353 [formerly 13643af7a3df0c81adcb6ca8873e4977de88be48]
    Former-commit-id: 93e2a60c56e535b51112d74ba4a041f7e349d80a

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,11 +1,12 @@
+const CONTRAST_BREAKPOINT = 86;
+
 const getContrastColor = (rgb: Array<number>) => {
   const mean = rgb.reduce((s, b) => s + b, 0) / 3;
-  return (mean > 86) ? [0, 0, 0] : [255, 255, 255];
+  return (mean > CONTRAST_BREAKPOINT) ? [0, 0, 0] : [255, 255, 255];
 };
 
 const setBodyColorTheme = (theme: string) => {
   const { body } = document;
-  console.debug('theme', theme, body);
   body.setAttribute('data-page-color', theme);
 };
 
