c86031aa641e9a6c829b4901a8d5a309b5007f9a
Jonas Ohrstrom <ohrstrom@gmail.com>
2021-12-08 17:49:50 +0100

    Implementing docker compose backing services.

--- core/settings/development.py
+++ core/settings/development.py
@@ -1,95 +1,30 @@
+import environ
 from .base import *
 
-INTERNAL_IPS = ("127.0.0.1",)
+SITE_URL = "http://local.next.openbroadcast.ch:3000"
 
-DEBUG = True
-
-SITE_URL = "http://next.openbroadcast.ch:3000"
 
 ##################################################################
-# db
+# storage & media
 ##################################################################
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "ch_openbroadcast_next_local",
-        "USER": "",
-        "HOST": "",
-    },
-}
-
-# TEMPLATES[0]["OPTIONS"]["loaders"] = [
-#     "django.template.loaders.filesystem.Loader",
-#     "django.template.loaders.app_directories.Loader",
-# ]
-
 DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+MEDIA_ROOT = PROJECT_ROOT / "data" / "media"
+MEDIA_URL = ""
 
+INSTALLED_APPS += []
 
-##################################################################
-# cache
-##################################################################
-CACHES = {
-    "default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"},
-    # 'default': {
-    #     'BACKEND': 'django_redis.cache.RedisCache',
-    #     'LOCATION': 'redis://127.0.0.1:6379/7',
-    #     'OPTIONS': {
-    #         'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-    #     },
-    # },
-}
-
-# SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
-# SESSION_CACHE_ALIAS = 'default'
-
-##################################################################
-# email
-##################################################################
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = ''
-# EMAIL_HOST_USER = ''
-# EMAIL_HOST_PASSWORD = ''
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-
-INSTALLED_APPS += [
-    # "dev",
-    # 'debug_toolbar',
-    # "django_extensions",
-]
-
-MIDDLEWARE += [
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
-]
-
-# DEBUG_TOOLBAR_PANELS = [
-#     #'cachalot.panels.CachalotPanel',
-# ]
-
-# this fixes strange behaviour when running app through gunicorn
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-
-
-WERKZEUG_DEBUG_PIN = "off"
-
-DEVSERVER_MODULES = []
-
-IPYTHON_ARGUMENTS = ["--debug"]
+MIDDLEWARE += []
 
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-    "root": {"level": "WARNING", "handlers": ["console"]},
+    "root": {
+        "level": "WARNING",
+        "handlers": ["console"],
+    },
     "formatters": {
         "default": {
-            "format": "%(levelname)s %(name)s:%(lineno)s %(message)s",
-        },
-        "verbose": {
-            # "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s",
-            "format": "%(asctime)s %(levelname)s\t%(name)s\t%(funcName)s:%(lineno)s - %(message)s",
+            "format": "%(levelname)-8s %(name)s\t%(message)s",
         },
     },
     "handlers": {
@@ -106,31 +41,21 @@ LOGGING = {
             "propagate": False,
         },
         "django.request": {
-            "level": "DEBUG",
-            "handlers": ["console"],
-            "propagate": False,
-        },
-        "geoip": {
-            "level": "DEBUG",
-            "handlers": ["console"],
-            "propagate": False,
-        },
-        "catalog": {
-            "level": "DEBUG",
-            "handlers": ["console"],
-            "propagate": False,
-        },
-        "sync": {
-            "level": "DEBUG",
-            "handlers": ["console"],
-            "propagate": False,
-        },
-        "raven": {
             "level": "INFO",
             "handlers": ["console"],
             "propagate": False,
         },
-        "sentry.errors": {
+        "geoip": {
+            "level": "INFO",
+            "handlers": ["console"],
+            "propagate": False,
+        },
+        "catalog": {
+            "level": "INFO",
+            "handlers": ["console"],
+            "propagate": False,
+        },
+        "sync": {
             "level": "INFO",
             "handlers": ["console"],
             "propagate": False,
---
+++ docker/media-encoder/main.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import sys
+import time
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+ENCODER_PATH = os.path.join(PROJECT_ROOT, "services", "media-encoder")
+sys.path.insert(0, ENCODER_PATH)
+
+try:
+    import encoder
+except ImportError:
+    sys.stderr.write(f"encoder module not in PATH")
+    sys.exit(1)
+
+
+def encode_dir_to_dash(src_dir, dst_dir):
+    sys.stdout.write(f"encode: {src_dir} > {dst_dir}\n")
+    if src := next(
+        (f for f in src_dir.iterdir() if f.is_file() and f.stem == "master"),
+        None,
+    ):
+        os.makedirs(dst_dir / "dash", exist_ok=True)
+        dst = dst_dir / "dash" / "manifest.mpd"
+        encoder.encode_dash(src=str(src.absolute()), dst=str(dst.absolute()))
+
+
+def run(master_dir, encoded_dir, force=False):
+
+    num_encoded = 0
+    num_skipped = 0
+    master_dirs = [d for d in master_dir.iterdir() if d.is_dir() and len(d.stem) == 8]
+
+    for src_dir in master_dirs:
+        dst_dir = encoded_dir / src_dir.stem
+        if dst_dir.is_dir() and not force:
+            num_skipped += 1
+            continue
+        encode_dir_to_dash(src_dir, dst_dir)
+        num_encoded += 1
+
+    return num_encoded, num_skipped
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "master_dir",
+        type=Path,
+        help="master directory",
+    )
+    parser.add_argument(
+        "encoded_dir",
+        type=Path,
+        help="encoded directory",
+    )
+    parser.add_argument(
+        "-i",
+        "--interval",
+        type=int,
+    )
+    args = parser.parse_args()
+
+    force = False  # TODO: add as flag
+    master_dir = args.master_dir
+    encoded_dir = args.encoded_dir
+
+    if not master_dir.is_dir():
+        sys.stderr.write(f"master_dir does not exist: {master_dir}")
+        sys.exit(1)
+
+    if not encoded_dir.is_dir():
+        sys.stderr.write(f"encoded_dir does not exist: {encoded_dir}")
+        sys.exit(1)
+
+    while args.interval:
+        num_encoded, num_skipped = run(master_dir, encoded_dir, force)
+        sys.stdout.write(f"files encoded: {num_encoded} - skipped: {num_skipped} - re-scan in {args.interval} seconds\n")
+        time.sleep(args.interval)
+    else:
+        run(master_dir, encoded_dir, force)
