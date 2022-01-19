46bccfc56b0333335b1cfec64e056ef52cbfcd67
Jonas Ohrstrom <ohrstrom@gmail.com>
2022-01-19 16:20:31 +0100

    Implemented event (player) models.
    Former-commit-id: bc37fab738a912ee3ca28c462d98e8482faf51c7
    Former-commit-id: dc8e90ad351b0fb8f4f9a61da00b4984f5516785

--- core/stats/models.py
+++ core/stats/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class PlayerEvent(models.Model):
+    time = models.DateTimeField(
+        db_index=True,
+    )
+    state = models.CharField(
+        max_length=32,
+        db_index=True,
+    )
+    obj_key = models.CharField(
+        max_length=32,
+        db_index=True,
+    )
+    source = models.CharField(
+        max_length=32,
+        db_index=True,
+    )
+    user_identity = models.CharField(
+        max_length=64,
+        db_index=True,
+    )
+    device_key = models.CharField(
+        max_length=64,
+        db_index=True,
+    )
+
+    class Meta:
+        app_label = "stats"
+        verbose_name = "Player event"
+        ordering = ["-time"]
+        db_table = "stats_player_event"
+        # managed = False
--- core/user_identity/middleware.py
+++ core/user_identity/middleware.py
@@ -1,6 +1,6 @@
 import logging
 
-from .utils import get_user_identity
+from .utils import get_user_identity, get_device_key
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +12,8 @@ class UserIdentityMiddleware:
     def __call__(self, request):
 
         request.user_identity = get_user_identity(request)
+        request.device_key = get_device_key(request)
         response = self.get_response(request)
         response["X-User-Identity"] = request.user_identity
+        response["X-Device-Key"] = request.device_key
         return response
