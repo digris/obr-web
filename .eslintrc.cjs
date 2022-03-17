dcb78a6e9ec35ddf4e78941c37955da9e5a8505a
Jonas Ohrstrom <ohrstrom@gmail.com>
2022-03-17 21:51:57 +0100

    Configured eslint for transition.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,61 @@ module.exports = {
   "root": true,
   "extends": [
     "plugin:vue/vue3-essential",
-    // "eslint:recommended",
+    // ported from previous webpack setup
+    // '@vue/airbnb',
+    // '@vue/typescript',
+    // below the vue3 suggested ones
+    "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier"
   ],
+  "rules": {
+    // ignores during transition to vite
+    "vue/multi-word-component-names": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    //
+    "arrow-body-style": "off",
+    "no-shadow": "off",
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/no-shadow": [
+      "error"
+    ],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error"
+    ],
+    // "vue/max-attributes-per-line": [
+    //   "error",
+    //   {
+    //     "singleline": {
+    //       "max": 1,
+    //       "allowFirstLine": false
+    //     },
+    //     "multiline": {
+    //       "max": 1,
+    //       "allowFirstLine": false
+    //     }
+    //   }
+    // ],
+    "vue/html-closing-bracket-newline": [
+      "error",
+      {
+        "singleline": "never",
+        "multiline": "always"
+      }
+    ],
+    "vue/html-indent": [
+      "error",
+      2,
+      {
+        "attribute": 1,
+        "baseIndent": 1,
+        "closeBracket": 0,
+        "alignAttributesVertically": true,
+        "ignores": []
+      }
+    ]
+  },
   "env": {
     "vue/setup-compiler-macros": true
   },
