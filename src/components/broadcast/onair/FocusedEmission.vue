d9b6a654eb713938f79a2faa659355018a0115c5
Jonas Ohrstrom <ohrstrom@gmail.com>
2022-03-17 15:38:43 +0100

    Implemented dynamic color theme for main navigation.

--- src/components/broadcast/onair/FocusedEmission.vue
+++ src/components/broadcast/onair/FocusedEmission.vue
@@ -78,9 +78,9 @@ export default defineComponent({
   @include typo.large;
 }
 a {
+  transition: color, background-color 200ms;
   @include responsive.on-hover {
     @include live-color.bg-inverse(0.1);
-    transition: color, background-color 200ms;
   }
 }
 </style>
