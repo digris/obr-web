0f373130fccc10aaca5991171200ae11ace546c2
Jonas Ohrstrom <ohrstrom@gmail.com>
2022-07-01 11:30:26 +0200

    refactor: fix code formatting

--- src/components/ui/audio/AudioSpectrumB.vue
+++ src/components/ui/audio/AudioSpectrumB.vue
@@ -1,12 +1,12 @@
 <script lang="ts">
-import { defineComponent, ref, computed, watch, onMounted } from "vue";
+import { defineComponent, ref, computed, onMounted } from "vue";
 import type { AudioAnalyser } from "@/player/analyser";
 
 const drawCanvas = async (
   ctx: CanvasRenderingContext2D,
   width: number,
   height: number,
-  bins: Uint8Array,
+  bins: Uint8Array
 ) => {
   ctx.canvas.width = width;
   ctx.canvas.height = height;
@@ -18,7 +18,7 @@ const drawCanvas = async (
   const w = barWidth;
   for (let i = 0; i < numBins; i++) {
     const value = bins[i] * heightFactor;
-    const x = i * (barWidth * 1.5) + (barWidth / 3);
+    const x = i * (barWidth * 1.5) + barWidth / 3;
     const h = value;
     ctx.fillStyle = "rgb(0,0,0)";
     ctx.fillRect(x, y, w, h);
@@ -55,7 +55,6 @@ export default defineComponent({
         return;
       }
       const ctx = canvas.value?.getContext("2d");
-
 
       const getDrawData = (analyser: AudioAnalyser) => {
         // we just need one channel here
