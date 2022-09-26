b6c388c8167579a4f26ab2b3a8256ba9588d0ce8
Jonas Ohrstrom <ohrstrom@gmail.com>
2022-09-26 21:14:22 +0200

    refactor: fix country serializer

--- src/typings/api/models/PatchedAddressRequest.ts
+++ src/typings/api/models/PatchedAddressRequest.ts
@@ -2,15 +2,11 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import type { BlankEnum } from './BlankEnum';
-import type { CountryEnum } from './CountryEnum';
-import type { NullEnum } from './NullEnum';
-
 export type PatchedAddressRequest = {
     line1?: string;
     line2?: string;
     postalCode?: string;
     city?: string;
-    country?: (CountryEnum | BlankEnum | NullEnum) | null;
+    country?: string;
 };
 
