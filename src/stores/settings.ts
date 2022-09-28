94cef660ab6997cac0e6655f37fa3d7d0e1ac877
Jonas Ohrstrom <ohrstrom@gmail.com>
2022-09-28 15:58:31 +0200

    refactor: (re)move legacy "onair" version

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -16,10 +16,15 @@ const getDefaultLanguage = () => {
 
 export const useSettingsStore = defineStore("settings", {
   state: (): State => ({
+    // @ts-ignore
     theme: useStorage("settings/theme", null),
+    // @ts-ignore
     volume: useStorage("settings/player/volume", 100),
+    // @ts-ignore
     shuffleMode: useStorage("settings/player/shuffleMode", false),
+    // @ts-ignore
     maxBandwidth: useStorage("settings/stream/maxBandwidth", 200000),
+    // @ts-ignore
     locale: useStorage("settings/locale", getDefaultLanguage()),
   }),
   getters: {
