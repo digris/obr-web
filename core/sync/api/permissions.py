e43530e832bd9a78a43342610e31c2e8b5f5909e
Jonas Ohrstrom <ohrstrom@gmail.com>
2022-11-25 13:59:15 +0100

    refactor: add API based schedule sync
    as cloud scheduler now supports HTTP request we can remove the "pub-sub-bridge" hack

--- core/sync/api/permissions.py
+++ core/sync/api/permissions.py
@@ -6,9 +6,6 @@ SYNC_PERMISSION = "account.api_sync_webhooks"
 
 
 class SyncAPIPermission(BasePermission):
-    """
-    Global permission check for blocked IPs.
-    """
 
     def has_permission(self, request, view):
         return request.user.has_perm(SYNC_PERMISSION)
