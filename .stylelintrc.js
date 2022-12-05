3724fdab8bfeace066be84292f9bdfd6cd0114d3
Jonas Ohrstrom <ohrstrom@gmail.com>
2022-12-05 12:35:02 +0100

    refactor: add stylelint configuration (scss)

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,24 +1,26 @@
 module.exports = {
-  extends: [
-    'stylelint-scss',
-    'stylelint-config-recommended',
-    'stylelint-config-rational-order',
-  ],
-  rules: {
-    'at-rule-no-unknown': [true, {
-      ignoreAtRules: ['debug', 'warn', 'function', 'if', 'else', 'use', 'each', 'include', 'extend', 'mixin', 'return', 'for'],
-    }],
-    indentation: 2,
-    'color-hex-case': 'lower',
-    'plugin/rational-order': [true, {
-      'border-in-box-model': false,
-      'empty-line-between-groups': false,
-    }],
-    'selector-pseudo-element-no-unknown': [true, {
-      ignorePseudoElements: ['deep'],
-    }],
-    'selector-pseudo-class-no-unknown': [true, {
-      ignorePseudoClasses: ['deep'],
-    }],
-  },
-};
+    // root: true,
+    plugins: [
+      'stylelint-order',
+      'stylelint-scss',
+    ],
+    extends: [
+        'stylelint-config-standard',
+        'stylelint-config-standard-scss',
+        'stylelint-config-recommended-vue',
+        'stylelint-config-recommended-vue/scss',
+        'stylelint-config-prettier',
+    ],
+    rules: {
+        'scss/double-slash-comment-whitespace-inside': 'always',
+        'order/properties-order': ['top', 'position', 'height', 'width',],
+        'selector-pseudo-class-no-unknown': [
+          true,
+          {
+            ignorePseudoClasses: ['deep'],
+          }
+        ]
+    },
+    // customSyntax: 'postcss-html',
+    ignoreFiles: ['**/*.tsx', '**/*.ts',],
+}
