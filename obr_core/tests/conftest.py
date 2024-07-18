e29d12e2b361132dab01a7109e9adef6d21afd95
Jonas Ohrstrom <ohrstrom@gmail.com>
2024-07-18 16:08:20 +0200

    refactor: format test (black)

--- obr_core/tests/conftest.py
+++ obr_core/tests/conftest.py
@@ -1,9 +1,11 @@
-import pytest
 from django.conf import settings
+
+import pytest
+
 
 def pytest_collectreport(report):
     if report.failed:
         raise pytest.UsageError("Errors during collection, aborting")
 
 
-settings.CDN_POLICY_DOMAIN="openbroadcast.ch"
+settings.CDN_POLICY_DOMAIN = "openbroadcast.ch"
