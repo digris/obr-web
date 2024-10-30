376cdb10f0cc7dc731f8171c2a56d22dfb6aa14f
Jonas Ohrstrom <ohrstrom@gmail.com>
2024-10-30 17:38:43 +0100

    refactor: add calculated_duration_s (post-process)

--- obr_core/common/cache/decorators.py
+++ obr_core/common/cache/decorators.py
@@ -11,8 +11,8 @@ def ignorable_cache_page(timeout):
         def _wrapped_view(request, *args, **kwargs):
             if IGNORE_CACHE_HEADER in request.headers:
                 return view_func(request, *args, **kwargs)
-            else:
-                return cache_page(timeout)(view_func)(request, *args, **kwargs)
+
+            return cache_page(timeout)(view_func)(request, *args, **kwargs)
 
         return _wrapped_view
 
